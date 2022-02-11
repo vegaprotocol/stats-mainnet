30a1860c73a733f7a9a684b3a451f9f43a1615db
sam-keen <samuel.kleinmann@gmail.com>
2022-02-11 17:53:17 +0000

    Re-adding app file to fix naming issue

---
+++ src/app.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./lib/apollo-client";
+import { Header } from "./components/header";
+import { StatsTable } from "./components/statsTable";
+
+function App() {
+  return (
+    <div className="w-screen h-screen grid bg-white dark:bg-black text-neutral-900 dark:text-neutral-200">
+      <div className="layout-grid w-screen justify-self-center">
+        <ApolloProvider client={client}>
+          <Header />
+          <StatsTable />
+        </ApolloProvider>
+      </div>
+    </div>
+  );
+}
+
+export default App;
